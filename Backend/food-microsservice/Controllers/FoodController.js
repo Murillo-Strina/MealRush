2859a48e09bb96dd3b4e2c98daaca771b19025bd
Vinicius <vinicius.gdechechi@gmail.com>
2025-04-14 20:59:04 -0300

    Adiciona metodo e endpoint para recuperar comida por id

--- Backend/food-microsservice/Controllers/FoodController.js
+++ Backend/food-microsservice/Controllers/FoodController.js
@@ -1,14 +1,32 @@
-const foodService = require('../services/FoodService');
+const foodService = require('../Services/FoodService.js');
 
 class FoodController {
 
     async GetAllFoods(req, res) {
         try {
             const foods = await foodService.FindAll();
-            res.status(200).json(foods); // Status 200 para sucesso
+
+            if(foods.length === 0){
+                return res.status(404).json({'Message': 'Nenhuma comida encontrada'}); 
+            }
+            res.status(200).json(foods); 
         } catch (err) {
             console.error("Erro ao buscar os alimentos:", err);
-            res.status(500).send("Erro ao buscar os dados"); // Status 500 para erro no servidor
+            res.status(500).send("Erro ao buscar os dados"); 
+        }
+    }
+
+    async GetFoodById(req, res){
+        try {
+            const id = req.params.id;
+            const food = await foodService.FindById(id);
+            if(food.length === 0){  
+                return res.status(404).json({'Message': 'A comida com id ' + id + ' não foi encontrada'}); 
+            }
+            res.status(200).json(food); 
+        } catch (err) {
+            console.error("Erro ao buscar os alimentos:", err);
+            res.status(500).send("Erro ao buscar os dados"); 
         }
     }
 }
