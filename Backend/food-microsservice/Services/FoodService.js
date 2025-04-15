58689570004ca186cf12aeabaf49daf60f97c493
MURILLO PENHA STRINA <22.00730-0@maua.br>
2025-04-15 16:50:40 -0300

    feat: implementa metodos create, update, findbyname, delete no service e endpoints

--- Backend/food-microsservice/Services/FoodService.js
+++ Backend/food-microsservice/Services/FoodService.js
@@ -11,6 +11,15 @@ class FoodService {
         }
     }
 
+    async FindByName(name) {
+        try {
+            const food = await db.promise().query("SELECT * FROM foods where name = ?", [name]);
+            return food[0];
+        } catch (err) {
+            throw err;
+        }
+    }
+
     async FindById(id) {
         try {
             const food = await db.promise().query("SELECT * FROM foods WHERE id = ?", [id]);
@@ -19,6 +28,35 @@ class FoodService {
             throw err;
         }
     }
+
+    async Delete(id) {
+        try {
+            await db.promise().query("DELETE FROM foods WHERE id = ?", [id]);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async Create(name, calories, carbs, proteins, fats, weight, imageUrl) {
+        try {
+            return await db.promise().query("INSERT INTO foods(name, calories, carbs, proteins, fats, weight, imageUrl) values (?, ?, ?, ?, ?, ?, ?)",
+                 [name, calories, carbs, proteins, fats, weight, imageUrl]);
+
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async Update(id, name, calories, carbs, proteins, fats, weight, imageUrl) {
+        try {
+            return await db.promise().query("UPDATE foods SET name = ?, calories = ?, carbs = ?, proteins = ?, fats = ?, weight = ?, imageUrl = ? where id = ?"
+                ,[name, calories, carbs, proteins, fats, weight, imageUrl, id]);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+
 }
 
 module.exports = new FoodService();
