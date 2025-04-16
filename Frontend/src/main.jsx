4c497c83d64233e6e635ef31177b909460b40165
Lucas Crapino <lucascrapino@gmail.com>
2025-04-16 18:47:47 -0300

    feat: adiciona tela de menu principal com barra de navegacao

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Machine from './components/Machine.jsx'
+import MainMenu from './pages/MainMenu.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Machine />
+    <MainMenu />
   </StrictMode>,
 )
---
+++ Frontend/src/pages/MainMenu.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from "../assets/images/logo_mealrush.jpeg";
+export default class MainMenu extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-black" style={{ background: "green" }}>
+        <div className="container-fluid position-relative">
+            <div>
+                <img src={logo} style={{width:85, marginRight:800}}/>
+            </div>
+          <div className="position-absolute start-50 translate-middle-x">
+            <a className="navbar-brand text-white" style={{fontFamily:"'Comic Sans MS', cursive, sans-serif", fontSize: 50}}>MealRush</a>
+          </div>
+          
+          <button 
+            className="navbar-toggler ms-auto"
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          
+          <div className="collapse navbar-collapse"style={{fontSize:20}} id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active text-white" href="#">Sobre nós</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">Alimentos</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">Parcerias</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">Simulação</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
