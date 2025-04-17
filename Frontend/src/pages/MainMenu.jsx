66fa54f26434192bfd628637e143c9e077190fc9
Lucas Crapino <lucascrapino@gmail.com>
2025-04-17 12:52:27 -0300

    feat: adiciona imagem de maquina de venda

--- Frontend/src/pages/MainMenu.jsx
+++ Frontend/src/pages/MainMenu.jsx
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
 import logo from "../assets/images/logo_mealrush.jpeg";
+import vendingMachine from "../assets/images/vending_magine.png";
 export default class MainMenu extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-black" style={{ background: "green" }}>
-        <div className="container-fluid position-relative">
+      <>
+        <nav className="navbar navbar-expand-lg navbar-black" style={{ background: "green" }}>
+          <div className="container-fluid position-relative">
             <div>
-                <img src={logo} style={{width:85, marginRight:800}}/>
+              <img src={logo} style={{ width: 85, marginRight: 800 }} />
             </div>
-          <div className="position-absolute start-50 translate-middle-x">
-            <a className="navbar-brand text-white" style={{fontFamily:"'Century Gothic', sans-serif", fontSize: 50}}>MealRush</a>
+            <div className="position-absolute start-50 translate-middle-x">
+              <a className="navbar-brand text-white" style={{ fontFamily: "'Century Gothic', sans-serif", fontSize: 50 }}>MealRush</a>
+            </div>
+
+            <button
+              className="navbar-toggler ms-auto"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" style={{ fontSize: 20 }} id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link active text-white" href="#">Sobre nós</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">Alimentos</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">Parcerias</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">Simulação</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          
-          <button 
-            className="navbar-toggler ms-auto"
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          
-          <div className="collapse navbar-collapse"style={{fontSize:20}} id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active text-white" href="#">Sobre nós</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Alimentos</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Parcerias</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Simulação</a>
-              </li>
-            </ul>
-          </div>
+        </nav>
+        <div>
+          <img src={vendingMachine} />
         </div>
-      </nav>
+      </>
     );
   }
 }
