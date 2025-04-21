c2f582129eaaeabad35ab8ba289903bd307a5a28
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2025-04-21 15:53:54 -0300

    chore: minor tweaks to files (no functional changes)

---
+++ Frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { motion } from 'framer-motion';
+import logo from "../assets/images/logo_mealrush.jpeg";
+
+export default class AdminPage extends Component {
+}
