281cbfac0160eb937b1f8821d364f6e7512cf5eb
Felipe Sakae <fosakae@icloud.com>
2025-05-17 09:08:23 -0300

    feat: add login screen

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,9 +4,10 @@ import Machine from './components/Machine.jsx'
 import MainMenu from './pages/MainMenu.jsx'
 import MachineScreen from './pages/MachineScreen.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import LoginScreen from './pages/LoginScreen.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MachineScreen machine={0o1}/>
+    <LoginScreen/>
   </StrictMode>,
 )
