f9f05f66f60bda1e4d8f61b3124068b2f0637713
Vinicius Garcia Imendes Dechechi <vinicius.gdechechi@gmail.com>
2025-05-20 17:14:41 -0300

    feat: rotas da api

--- Backend/food-microsservice/Routes/routes.js
+++ Backend/food-microsservice/Routes/routes.js
@@ -1,5 +1,5 @@
-import { Router } from "express";
-const router = Router();
+import express from 'express';
+const router = express.Router();
 import foodController from '../Controllers/FoodController.js';
 
 
