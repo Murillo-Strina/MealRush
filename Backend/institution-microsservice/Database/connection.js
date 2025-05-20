793346a1a4207578f7506f8eda1fdee61421ef49
Vinicius Garcia Imendes Dechechi <vinicius.gdechechi@gmail.com>
2025-05-20 17:14:30 -0300

    feat: Serviço de Instituição

---
+++ Backend/institution-microsservice/Database/connection.js
@@ -0,0 +1,27 @@
+import { createConnection } from 'mysql2';
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const connection = createConnection({
+  host: 'trabalhoprog.cl40iigyy6en.sa-east-1.rds.amazonaws.com',
+  port: process.env.DB_PORT || 3306, 
+  user: 'admin',
+  password: process.env.DB_PASSWORD,
+  database: 'mealrush'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados!');
+});
+
+export default connection;
---
+++ Backend/institution-microsservice/index.js
@@ -0,0 +1,17 @@
+import bodyParser from "body-parser";
+import express from "express";
+const app = express();
+import router from "./Routes/routes.js";
+import cors from "cors";
+
+const { urlencoded, json } = bodyParser;
+
+app.use(cors());
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.use("/", router);
+
+app.listen(3005, () => {
+  console.log("Institution: Servidor rodando na porta 3005");
+});
