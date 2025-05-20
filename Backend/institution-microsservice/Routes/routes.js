ce9574eb0c0a056e79f74a70c0bc2d1fb4349909
Vinicius Garcia Imendes Dechechi <vinicius.gdechechi@gmail.com>
2025-05-20 17:14:56 -0300

    feat: routes e Controller de instituição

---
+++ Backend/institution-microsservice/Routes/routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import institutionController from '../Controllers/institutionController.js';
+
+const router = express.Router();
+
+router.get('/institutions', institutionController.GetAllInstitutions);
+router.get('/institutions/:id', institutionController.GetInstitutionById);
+router.post('/institutions', institutionController.Create);
+router.put('/institutions/:id', institutionController.Update);
+router.delete('/institutions/:id', institutionController.Delete);
+
+export default router;
