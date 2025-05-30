78922e8c7762fb3176ae00c62d08de50153c6f03
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-05-30 14:37:28 -0300

    feat: adiciona fila propria para o mss de comida

---
+++ Backend/food-microsservice/EventHandler/FoodEventHandler.js
@@ -0,0 +1,11 @@
+export async function handleFoodEvent(routingKey, data){
+    switch(routingKey){
+        case 'food.created':
+            console.log('food created:',data);
+        case 'food.updated':
+            console.log('food updated:',data);
+        case 'food.deleted':
+            console.log('food deleted:',data);
+            break;
+    }
+}
