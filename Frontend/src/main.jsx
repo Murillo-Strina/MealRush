201b4116a55a994453b68e318bd6a0a6439c4d3a
Vinicius Garcia Imendes Dechechi <vinicius.gdechechi@gmail.com>
2025-05-31 11:07:16 -0300

    feat: ajustes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -10,8 +10,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import ChatBot from './pages/ChatBot.jsx'
-import Admin from './pages/AdminScreen.jsx'
+import AdminScreen from './pages/AdminScreen.jsx'
 import ManagementScreen from './pages/ManagementScreen.jsx'
+
+import ForgottenPasswordScreen from './pages/ForgottenPasswordScreen.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,13 +21,13 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<MainMenu />} />
         <Route path="/login" element={<LoginScreen />} />
-        {/* <Route path="/forgotten-password" element={<ForgottenPasswordScreen />} /> */}
+        <Route path="/forgotten-password" element={<ForgottenPasswordScreen />} />
         <Route path="/machine" element={<MachineScreen />} />
         <Route path="/simulation" element={<Machine />} />
         <Route path="/chatbot" element={<ChatBot />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/management" element={<ManagementScreen />} />
-        <Route path="/feedback" element={<FeedbackScreen />} />
+        <Route path="/admin" element={<PrivateRoute><AdminScreen /></PrivateRoute>} />
+        <Route path="/management" element={<PrivateRoute> <ManagementScreen /></PrivateRoute>} />
+        
       </Routes>
     </BrowserRouter>
   </StrictMode>,
