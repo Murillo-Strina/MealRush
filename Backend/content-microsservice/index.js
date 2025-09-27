4a4d99d49187deefc1b398a3e112b7b147190245
Lucas Crapino <lucascrapino@gmail.com>
2025-09-27 17:04:53 -0300

    feat: adiciona conteiner docker ao microsservico de conteudo

--- Backend/content-microsservice/index.js
+++ Backend/content-microsservice/index.js
@@ -6,8 +6,13 @@ import dotenv from "dotenv";
 import { initRabbitMQ } from "../event-bus/index.js";
 import { consumeEvent } from "../event-bus/index.js";
 import { handleContentEvent } from "./EventHandler/ContentEventHandler.js";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const app = express();
 const { urlencoded, json } = bodyParser;
 
