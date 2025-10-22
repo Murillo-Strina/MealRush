f2f750ba710fadd5281afc2a38f448036094154a
Sakaef03 <fosakae@icloud.com>
2025-10-22 14:54:28 -0300

    feat: adiciona tela de login

--- Frontend_Mobile/lib/src/app.dart
+++ Frontend_Mobile/lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,12 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu App',
+      title: 'MealRush Club',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
---
+++ Frontend_Mobile/lib/src/screens/login_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 24.0,
+                ),
+                child: Center(
+                  child: Container(
+                    constraints: BoxConstraints(maxWidth: 400),
+
+                    padding: const EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: 'Insira seu email',
+                            hintStyle: TextStyle(color: Colors.white70),
+                            icon: Icon(
+                              Icons.email_outlined,
+                              color: Colors.white,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+
+                        Divider(
+                          color: Colors.white54,
+                          thickness: 1,
+                          height: 20,
+                        ),
+
+                        TextFormField(
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: 'Insira sua senha',
+                            hintStyle: TextStyle(color: Colors.white70),
+                            icon: Icon(
+                              Icons.lock_outline_rounded,
+                              color: Colors.white,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+
+                        SizedBox(height: 15),
+
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  foregroundColor: Colors.orange,
+                                  minimumSize: Size(0, 48),
+                                ),
+                                onPressed: () {},
+                                child: Text('Entrar'),
+                              ),
+                            ),
+
+                            SizedBox(width: 10),
+
+                            Expanded(
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  foregroundColor: Colors.orange,
+                                  minimumSize: Size(0, 48),
+                                ),
+                                onPressed: () {},
+                                child: Text('Criar conta'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
