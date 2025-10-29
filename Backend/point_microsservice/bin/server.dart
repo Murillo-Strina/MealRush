297cbedd180acc6127d3edfd17018b92fc3a6f62
Lucas Crapino <lucascrapino@gmail.com>
2025-10-28 21:44:58 -0300

    feat: adiciona arquivos do mss de pontos e eventuais arquivos no gitignore

---
+++ Backend/point_microsservice/bin/server.dart
@@ -0,0 +1,5 @@
+import 'package:point_microsservice/point_service.dart' as point_microsservice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${point_microsservice.calculate()}!');
+}
---
+++ Backend/point_microsservice/test/server_test.dart
@@ -0,0 +1,8 @@
+import 'package:point_microsservice/point_service.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
